86cfaf4dfdcff60c0be71d6a7f0df4b0db7b416f
0xArtex <0xArtex@users.noreply.github.com>
2026-04-04 17:16:40 +0000

    Braille mascot matching character traits exactly

--- cli/mascot-data.ts
+++ cli/mascot-data.ts
@@ -1,19 +1,17 @@
-// Hand-crafted ASCII mascot matching the cube character
-// Uses only basic ASCII: . : + * # @ and box-drawing
-// Works in every terminal font
+// Hand-crafted braille Unicode mascot matching the cube character exactly
+// Traits: rounded square, two small button eyes at ~40% height,
+// U-shaped stepped smile at ~65% height, cream fill, black outline
+// 16 chars wide × 12 rows tall
 export const MASCOT_STIPPLE: string[] = [
-  "    .+#######+.    ",
-  "  +##############+  ",
-  " ##              ## ",
-  " ##              ## ",
-  " ##   .##. .##.  ## ",
-  " ##   #### ####  ## ",
-  " ##   '##' '##'  ## ",
-  " ##              ## ",
-  " ##              ## ",
-  " ##    ':::::    ## ",
-  " ##     ':::'    ## ",
-  " ##              ## ",
-  "  +##############+  ",
-  "    '+########+'    ",
+  "⠀⠀⢀⣤⣤⣤⣤⣤⣤⣤⣤⣤⡀⠀⠀",
+  "⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀",
+  "⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇",
+  "⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇",
+  "⢸⣿⣿⣿⣦⣤⣿⣿⣿⣤⣴⣿⣿⣿⡇",
+  "⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇",
+  "⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇",
+  "⢸⣿⣿⣿⣧⣀⣀⣀⣀⣼⣿⣿⣿⣿⡇",
+  "⢸⣿⣿⣿⣿⣿⠻⠟⣿⣿⣿⣿⣿⣿⡇",
+  "⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀",
+  "⠀⠀⠈⠛⠿⣿⣿⣿⣿⣿⡿⠛⠁⠀⠀",
 ];
